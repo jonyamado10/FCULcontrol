d609d26dc84b62e3ed08573a15655fe1c53ef228
João Amado <fc42191@alunos.di.fc.u.pt>
2018-02-22 21:34:11 +0000

    teste

--- application/views/db-connect-test.php
+++ application/views/db-connect-test.php
@@ -15,3 +15,21 @@ $serverName = "tcp:fcul-acm.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 echo "ok";
 
+$dbname = "fcul-acm"
+$sql = "SHOW TABLES FROM $dbname";
+$result = mysql_query($sql);
+
+if (!$result) {
+    echo "DB Error, could not list tables\n";
+    echo 'MySQL Error: ' . mysql_error();
+    exit;
+}
+
+while ($row = mysql_fetch_row($result)) {
+    echo "Table: {$row[0]}\n";
+}
+
+mysql_free_result($result);
+?>
+
+
