d93e1276debe2acaaecb16591168ea2ac39e8163
João Amado <fc42191@alunos.di.fc.u.pt>
2018-02-22 21:45:10 +0000

    teste

--- application/views/db-connect-test.php
+++ application/views/db-connect-test.php
@@ -1,5 +1,26 @@
 <?php
 // PHP Data Objects(PDO) Sample Code:
+echo "<table style='border: solid 1px black;'>";
+echo "<tr><th>Id</th><th>Firstname</th><th>Lastname</th></tr>";
+
+class TableRows extends RecursiveIteratorIterator { 
+    function __construct($it) { 
+        parent::__construct($it, self::LEAVES_ONLY); 
+    }
+
+    function current() {
+        return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
+    }
+
+    function beginChildren() { 
+        echo "<tr>"; 
+    } 
+
+    function endChildren() { 
+        echo "</tr>" . "\n";
+    } 
+} 
+
 try {
     $conn = new PDO("sqlsrv:server = tcp:fcul-acm.database.windows.net,1433; Database = fcul-acm", "ptiptr4", "PTIptr04");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
