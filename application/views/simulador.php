63f8f5852f11c8ec652c64742738b7dd6a66bf51
João Amado <fc42191@alunos.di.fc.u.pt>
2018-02-23 00:55:49 +0000

    simulador html css

--- application/views/simulador.php
+++ application/views/simulador.php
@@ -3,6 +3,7 @@
 <html lang="en">
 <head>
 	<title>Simulador</title>
+	<?php echo base_url("assets/vendor/bootstrap/css/bootstrap.min.css") ?>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->
