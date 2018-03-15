96d808625618deaf71f9a16713ce9c52f91c1c37
João Amado <fc42191@alunos.di.fc.u.pt>
2018-03-15 20:49:50 +0000

    admin dashboard

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,6 +1,5 @@
 <?php 
 
-
 class Login extends CI_Controller{
 	
 
@@ -10,18 +9,15 @@ class Login extends CI_Controller{
 		$this->form_validation->set_rules('email', 'email', 'required|trim|callback_validate_credentials');
 		$this->form_validation->set_rules('password', 'Password', 'required|trim|md5');
 		if($this->form_validation->run()){
-			 session_unset(); 
+			sess_destroy();
 
 			if($this->Users_model->is_aluno($this->input->post('email'))){
-				$data = array( 'is_logged_in'=> 1);
-				$userInfo = $this->Users_model->get_alunoInfo($this->input->post('email'));
-				$this->session->set_userdata($data);
-				$this->session->sess_expiration = '14400';// expires in 4 hour
-				redirect(base_url('Main/aluno'));
-				exit();
-            	print_r($this->session->userdata());
 
-            	echo "pagina aluno";
+				$userInfo = $this->Users_model->get_alunoInfo($this->input->post('email'));
+				$userInfo['is_logged_in_aluno'] = 1;
+				$this->session->set_userdata($userInfo);
+				$this->session->sess_expiration = '14400';// expires in 4 hour
+            redirect('Main/aluno');
             
 			}
 			else{// se não é aluno, é funcionario
