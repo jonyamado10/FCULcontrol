43517005c8a26f1110734e30f700e6c963308021
João Amado <fc42191@alunos.di.fc.u.pt>
2018-03-15 19:32:36 +0000

    admin dashboard

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,4 +1,5 @@
 <?php 
+session_start();
 
 class Login extends CI_Controller{
 	
--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Main extends CI_Controller {
