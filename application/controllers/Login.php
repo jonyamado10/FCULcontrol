fc0a05e6a07438aa730236e27c5e8b731dd238b9
João Amado <fc42191@alunos.di.fc.u.pt>
2018-03-15 20:58:00 +0000

    admin dashboard

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -17,7 +17,7 @@ class Login extends CI_Controller{
 				$userInfo['is_logged_in_aluno'] = 1;
 				$this->session->set_userdata($userInfo);
 				$this->session->sess_expiration = '14400';// expires in 4 hour
-            redirect('Main/aluno');
+            	redirect('Main/aluno');
             
 			}
 			else{// se não é aluno, é funcionario
