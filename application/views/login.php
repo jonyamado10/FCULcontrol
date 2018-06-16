a19a269acd57b134238a52b67980bff86605cefe
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-06-16 18:19:38 +0100

    nova app

--- application/views/login.php
+++ application/views/login.php
@@ -43,7 +43,7 @@
 
 			<div>
 				<button class="btn-show-contact100">
-					login-----<?php echo $_SERVER["REMOTE_ADDR"];?>
+					login<BR><?php echo $_SERVER["REMOTE_ADDR"];?>
 				</button>
 			</div>
 		</div>
