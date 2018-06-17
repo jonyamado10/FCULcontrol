3b380ad62e775265119f9dcd10e647e41b421649
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-06-17 21:13:47 +0100

    db failover

--- application/config/database.php
+++ application/config/database.php
@@ -93,27 +93,7 @@ $db['default'] = array(
 	'compress' => FALSE,
 	'stricton' => FALSE,
 	'failover' => array(
-					array(
-						'dsn'	=> '',
-						'hostname' => 'acessosdbserver2.database.windows.net,1433',
-						'username' => 'acessosadmin',
-						'password' => 'PTIptr04',
-						'database' => 'acessosDB',
-						'dbdriver' => 'sqlsrv',
-						'dbprefix' => '',
-						'pconnect' => FALSE,
-						'db_debug' => (ENVIRONMENT !== 'production'),
-						'cache_on' => FALSE,
-						'cachedir' => '',
-						'char_set' => 'utf8',
-						'dbcollat' => 'utf8_general_ci',
-						'swap_pre' => '',
-						'encrypt' => FALSE,
-						'compress' => FALSE,
-						'stricton' => FALSE,
-						'failover' => array(),
-						'save_queries' => TRUE
-					)
+
 	),
 	'save_queries' => TRUE
 );
