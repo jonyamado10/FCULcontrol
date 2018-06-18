298c2475fc94071079389b10b0afd8414e207a2a
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-06-18 21:53:43 +0100

    passwords

--- application/views/nav.php
+++ application/views/nav.php
@@ -231,7 +231,7 @@
         </li>
         <li class="nav-item" style=" margin:0 auto;">
           <a class="nav-link" style="color:white; cursor: default;">
-            <i class="fa fa-fw fa-user-tie"></i><?php echo "Olá ".$this->session->userdata('nome')?>
+            <i class="fa fa-fw fa-user-secret"></i><?php echo "Olá ".$this->session->userdata('nome')?>
           </a>
         </li>
         <li class="nav-item">
