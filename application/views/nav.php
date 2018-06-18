7a3ec2d72357b21ec65d7c032fde0299c45085db
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-06-18 23:50:53 +0100

    alertas

--- application/views/nav.php
+++ application/views/nav.php
@@ -209,11 +209,11 @@
               ?>
               <div class="dropdown-divider"></div>
               <a class="dropdown-item" href="#">
-                <span class="text-warning">
+                <span class="text-danger">
                   <strong>
-                    <i class="fa fa-long-arrow-alt-down fa-fw text-danger"></i>Possiveis Falhas</strong>
+                    <i class="fa fa-long-arrow-alt-down fa-fw"></i>Possiveis Falhas</strong>
                 </span>
-                <span class="small float-right text-success"><?php $sensor->porta; ?></span>
+                <span class="small float-right text-warning"><?php echo $sensor->porta; ?></span>
                 <div class="dropdown-message small">O sensor de <?php echo $sensor->sentido." da porta ".$sensor->porta;?> não <br>regista nenhum acesso desde -------</div>
               </a>
              <?php 
