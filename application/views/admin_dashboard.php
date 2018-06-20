f6e0f3cc9c393435f8f615af9626073511c8ef89
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-06-20 02:43:24 +0100

    admin toques finais

--- application/views/admin_dashboard.php
+++ application/views/admin_dashboard.php
@@ -104,6 +104,8 @@
       <!-- Example DataTables Card-->
 
     </div>
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
   <script type="text/javascript">
 function updateDiv() {
 
