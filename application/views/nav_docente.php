bb70543941c1fd4dd8426e49b67586bc953f40c4
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-06-20 23:56:11 +0100

    docente toques finais

--- application/views/nav_docente.php
+++ application/views/nav_docente.php
@@ -23,18 +23,7 @@
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
 
     <script src=<?php echo base_url("assets/vendor/jquery/jquery.min.js") ?>></script>
-    <script src=<?php echo base_url("assets/vendor/bootstrap/js/bootstrap.bundle.min.js") ?>></script>
-    <!-- Core plugin JavaScript-->
-    <script src=<?php echo base_url("assets/vendor/jquery-easing/jquery.easing.min.js") ?>></script>
-    <!-- Page level plugin JavaScript-->
-    <script src=<?php echo base_url("assets/vendor/chart.js/Chart.min.js") ?>></script>
-    <script src=<?php echo base_url("assets/vendor/datatables/jquery.dataTables.js") ?>></script>
-    <script src=<?php echo base_url("assets/vendor/datatables/dataTables.bootstrap4.js") ?>></script>
-    <!-- Custom scripts for all pages-->
-    <script src=<?php echo base_url("assets/js/sb-admin.min.js") ?>></script>
-    <!-- Custom scripts for this page-->
-    <script src=<?php echo base_url("assets/js/sb-admin-datatables.min.js") ?>></script>
-    <script src=<?php echo base_url("assets/js/sb-admin-charts.min.js") ?>></script>
+
 
 </head>
 
