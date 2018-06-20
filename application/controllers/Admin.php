881fae08c0be4c188fd87c3aa0834a3448f8e0b1
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-06-20 01:58:38 +0100

    admin toques finais

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -12,8 +12,9 @@ class Admin extends CI_Controller {
 	{
 		if($this->session->userdata('is_logged_in_admin')){
 			$this->load->model('Acessos_model');
-			$data['sensores'] = $this->Acessos_model->sensores_avariados();
-			$data1['num_sensores'] = $data['sensores'];
+			$sensores = $this->Acessos_model->sensores_avariados();
+			$data['sensores'] = $sensores;
+			$data1['num_sensores'] = $sensores;
 			$data1['num_acessos_hj'] = $this->Acessos_model->get_num_acessos_hj();
 			$data1['num_acessos_corrigidos_hj'] = $this->Acessos_model->get_num_acessos_corrigidos_hj();
 			$data1['num_alunos_nao_passou_cartao'] = sizeof($this->Acessos_model->get_num_vezes_aluno_nao_passou_cartao_24h());			
@@ -25,13 +26,13 @@ class Admin extends CI_Controller {
 			redirect('Main/login');
 		}
 	}
-	
+
 	public function dashboard()
 	{
 		if (!isset($_SERVER['HTTP_REFERER']))
 		{ redirect('Admin');}
 		$this->load->model('Acessos_model');
-		$data['num_sensores'] = sizeof($this->Acessos_model->sensores_avariados());
+		$data['num_sensores'] = $this->Acessos_model->sensores_avariados();
 		$data['num_acessos_hj'] = $this->Acessos_model->get_num_acessos_hj();
 		$data['num_acessos_corrigidos_hj'] = $this->Acessos_model->get_num_acessos_corrigidos_hj();
 		$data['num_alunos_nao_passou_cartao'] = sizeof($this->Acessos_model->get_num_vezes_aluno_nao_passou_cartao_24h());
