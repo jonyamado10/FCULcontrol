1dd7e4f0e89c9c7f3c1ba1a9b00c6ed420436d1f
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-06-20 23:35:52 +0100

    docente toques finais

--- application/models/Users_model.php
+++ application/models/Users_model.php
@@ -712,7 +712,7 @@ class Users_model extends CI_model{
 	  				"designacao" => $disciplina->designacao,
 	  				"turma" => $disciplina->turma,
                     "total_presencas" =>
-                    $this->get_num_total_presencas_disciplina_licenciatura($disciplina->id),
+                    $this->get_num_total_presencas_disciplina_licenciatura($disciplina->id)->n_presencas,
                     "total_presencas_possiveis" =>$this->get_num_aulas_disciplina_licenciatura($disciplina->id) * $this->get_num_alunos_inscritos_disciplina_licenciatura($disciplina->id)
 	  				);
 	  }
@@ -726,7 +726,7 @@ class Users_model extends CI_model{
 	  				"designacao" => $disciplina->designacao,
 	  				"turma" => $disciplina->turma,
                     "total_presencas" =>
-                    $this->get_num_total_presencas_disciplina_mestrado($disciplina->id),
+                    $this->get_num_total_presencas_disciplina_mestrado($disciplina->id)->n_presencas,
                     "total_presencas_possiveis" =>$this->get_num_aulas_disciplina_mestrado($disciplina->id) * $this->get_num_alunos_inscritos_disciplina_mestrado($disciplina->id)
 	  				);
 	  }
@@ -740,7 +740,7 @@ class Users_model extends CI_model{
 	  				"designacao" => $disciplina->designacao,
 	  				"turma" => $disciplina->turma,
                     "total_presencas" =>
-                    $this->get_num_total_presencas_disciplina_posgraduacao($disciplina->id),
+                    $this->get_num_total_presencas_disciplina_posgraduacao($disciplina->id)->n_presencas,
                     "total_presencas_possiveis" =>$this->get_num_aulas_disciplina_pos_graduacao($disciplina->id) * $this->get_num_alunos_inscritos_disciplina_pos_graduacao($disciplina->id)
 	  				);
 	  }
