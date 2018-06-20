2cf6230f0b1a796845c4cc97cd476a11ef40df88
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-06-20 02:26:30 +0100

    admin toques finais

--- application/views/admin_dashboard.php
+++ application/views/admin_dashboard.php
@@ -98,12 +98,7 @@
       </div>
       <!-- Area Chart Example-->
       <div class="card mb-3 acessosLive" >
-        <div class="card-header">
-          <i class="fa fa-area-chart"></i> Area Chart Example</div>
-        <div class="card-body">
-          <canvas id="myAreaChart" width="100%" height="30"></canvas>
-        </div>
-        <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
+
       </div>
       
       <!-- Example DataTables Card-->
