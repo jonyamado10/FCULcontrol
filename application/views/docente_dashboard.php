59937f258e984e70ed68edb87a2e0b606e0d6271
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-06-20 23:49:20 +0100

    docente toques finais

--- application/views/docente_dashboard.php
+++ application/views/docente_dashboard.php
@@ -43,11 +43,19 @@
           </div>
         </div>
         <div class="col-xl-3 col-sm-6 mb-3">
+          <?php if ($percentagem > 50){ ?>
           <div class="card text-white bg-success o-hidden h-100">
             <div class="card-body">
               <div class="card-body-icon">
                 <i class="fa fa-fw fa-thumbs-up"></i>
               </div>
+              <?php }else{ ?>
+              <div class="card text-white bg-danger o-hidden h-100">
+            <div class="card-body">
+              <div class="card-body-icon">
+                <i class="fa fa-fw fa-thumbs-down"></i>
+              </div>
+              <?php }?>
               <div class="mr-5"><?php echo $percentagem ?> % Média de assiduidade nas suas aulas!</div>
             </div>
             <a class="card-footer text-white clearfix small z-1" href="#">
@@ -59,7 +67,7 @@
           </div>
         </div>
         <div class="col-xl-3 col-sm-6 mb-3">
-          <div class="card text-white bg-danger o-hidden h-100">
+          <div class="card text-white bg-warning o-hidden h-100">
             <div class="card-body">
               <div class="card-body-icon">
                 <i class="fa fa-fw fa-support"></i>
