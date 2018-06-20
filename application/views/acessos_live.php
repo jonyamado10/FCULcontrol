b5a8180637f015fb88167dfbad7d2f672c36f8da
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-06-20 03:40:23 +0100

    admin toques finais

--- application/views/acessos_live.php
+++ application/views/acessos_live.php
@@ -26,7 +26,7 @@ $table['cols'] = array(
 foreach ($result as $row => $value) 
 {
  $sub_array = array();
- $datetime = explode(".", $row->hora]);
+ $datetime = explode(".", $row->hora);
  $sub_array[] =  array(
       "v" => 'Date(' . $datetime[0] . '000)'
      );
