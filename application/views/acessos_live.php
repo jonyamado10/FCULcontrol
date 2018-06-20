d9474363c1d25c40b31f90787b65e9cd542685da
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-06-20 03:49:35 +0100

    admin toques finais

--- application/views/acessos_live.php
+++ application/views/acessos_live.php
@@ -1,75 +1,82 @@
-<?php
-//index.php
-
-$rows = array();
-$table = array();
-
-$table['cols'] = array(
- array(
-  'label' => 'Date Time', 
-  'type' => 'datetime'
- ),
- array(
-  'label' => 'Nº Acessos', 
-  'type' => 'number'
- )
-);
-
-foreach ($acessos24 as $row) 
-{
- $sub_array = array();
- $datetime = explode(".", $row);
- $sub_array[] =  array(
-      "v" => 'Date(' . $datetime[0] . '000)'
-     );
- $sub_array[] =  array(
-      "v" => $acessos24[$row]
-     );
- $rows[] =  array(
-     "c" => $sub_array
-    );
-}
-$table['rows'] = $rows;
-$jsonTable = json_encode($table);
-
+<div class="container-fluid">
+      <!-- Breadcrumbs-->
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item">
+          <a href="#">Dashboard</a>
+        </li>
+        <li class="breadcrumb-item active">Gráficos</li>
+      </ol>
+      <!-- Example DataTables Card-->
+    <div class="card mb-3">
+              
+        <div class="card-header">
+          <i class="fa fa-area-chart"></i> Acessos ultimas 24h</div>
+        <div class="card-body">
+          <canvas id="acessos24" width="100%" height="30"></canvas>
+        </div>
+      
+        <div class="card-footer small text-muted"><?php date_default_timezone_set("Europe/Lisbon");
+echo "Atualizado pela última vez às: " . date("G:i");
 ?>
+      
+        </div>
+    </div>
+</div>
 
-
-<html>
- <head>
-  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-  <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-  <script type="text/javascript">
-   google.charts.load('current', {'packages':['corechart']});
-   google.charts.setOnLoadCallback(drawChart);
-   function drawChart()
-   {
-    var data = new google.visualization.DataTable(<?php echo $jsonTable; ?>);
-
-    var options = {
-     title:'Sensors Data',
-     legend:{position:'bottom'},
-     chartArea:{width:'95%', height:'65%'}
-    };
-
-    var chart = new google.visualization.LineChart(document.getElementById('line_chart'));
-
-    chart.draw(data, options);
-   }
-  </script>
-  <style>
-  .page-wrapper
-  {
-   width:1000px;
-   margin:0 auto;
+<script type="text/javascript">
+  <?php 
+  $js_array = json_encode(array_keys($acessos24));
+  echo "var javascript_array = ". $js_array . ";\n";
+  ?>
+var ctx = document.getElementById("acessos24");
+var myLineChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: javascript_array,
+    datasets: [{
+      label: "acessos",
+      lineTension: 0.3,
+      backgroundColor: "rgba(2,117,216,0.2)",
+      borderColor: "rgba(2,117,216,1)",
+      pointRadius: 5,
+      pointBackgroundColor: "rgba(2,117,216,1)",
+      pointBorderColor: "rgba(255,255,255,0.8)",
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: "rgba(2,117,216,1)",
+      pointHitRadius: 20,
+      pointBorderWidth: 2,
+      data:  <?php echo json_encode(array_values($acessos24));?>,
+    }],
+  },
+  options: {
+    scales: {
+      xAxes: [{
+        time: {
+          unit: 'hour'
+        },
+        gridLines: {
+          display: false
+        },
+        ticks: {
+          maxTicksLimit: 24
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          min: 0,
+          max: Math.max(...<?php echo json_encode(array_values($acessos24));?>) +100,
+          maxTicksLimit: 5
+        },
+        gridLines: {
+          color: "rgba(0, 0, 0, .125)",
+        }
+      }],
+    },
+    legend: {
+      display: false
+    }
   }
-  </style>
- </head>  
- <body>
-  <div class="page-wrapper">
-   <br />
-   <h2 align="center">Display Google Line Chart with JSON PHP & Mysql</h2>
-   <div id="line_chart" style="width: 100%; height: 500px"></div>
-  </div>
- </body>
-</html>
+});
+
+
+</script>
