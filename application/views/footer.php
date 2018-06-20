a026f801fd310c44f734fd45ae283204fbf944a2
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-06-20 04:24:44 +0100

    admin toques finais

--- application/views/footer.php
+++ application/views/footer.php
@@ -43,7 +43,6 @@
     <script src=<?php echo base_url("assets/js/sb-admin.min.js") ?>></script>
     <!-- Custom scripts for this page-->
     <script src=<?php echo base_url("assets/js/sb-admin-datatables.min.js") ?>></script>
-    <script src=<?php echo base_url("assets/js/sb-admin-charts.min.js") ?>></script>
     <script src=<?php echo base_url("assets/clockpicker/js/bootstrap-clockpicker.min.js") ?>></script>
 
 <script >
