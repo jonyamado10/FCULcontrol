4ebf26d6a3d17d9fafdb68a0d46819951f1b9c14
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-06-20 21:03:23 +0100

    docente toques finais

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://www.acessosfcul.cloudns.cc/';
+$config['base_url'] = 'www.acessosfcul.cloudns.cc/';
 
 /*
 |--------------------------------------------------------------------------
@@ -324,7 +324,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = 'ptiptr';
+$config['encryption_key'] = 'K5c4dr?yu@-6pEs6ek7na#r-yupu+aXE';
 
 /*
 |--------------------------------------------------------------------------
