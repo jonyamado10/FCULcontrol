06255cfe0ca4e66445d1ff532a3e546f6ef3c7b6
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-06-20 23:44:39 +0100

    docente toques finais

--- application/views/docente_dashboard.php
+++ application/views/docente_dashboard.php
@@ -46,7 +46,7 @@
           <div class="card text-white bg-success o-hidden h-100">
             <div class="card-body">
               <div class="card-body-icon">
-                <i class="fa fa-fw fa-shopping-cart"></i>
+                <i class="fa fa-fw fa-thumbs-up"></i>
               </div>
               <div class="mr-5"><?php $percentagem ?> % Média de assiduidade nas suas aulas!</div>
             </div>
