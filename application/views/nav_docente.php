e3f29c7e365f72af737495a40abef20f3e56078d
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-06-20 21:28:55 +0100

    docente toques finais

--- application/views/nav_docente.php
+++ application/views/nav_docente.php
@@ -138,7 +138,7 @@
       <ul class="navbar-nav ml-auto">
         <li class="nav-item" style=" margin:0 auto;">
           <a class="nav-link" style="color:white; cursor: default;">
-            <i class="fa fa-fw fa-user-secret"></i><?php echo "Olá ".$this->session->userdata('nome')?>
+            <i class="fa fa-fw fa-user-ninja"></i><?php echo "Olá ".$this->session->userdata('nome')?>
           </a>
         </li>
         <li class="nav-item">
