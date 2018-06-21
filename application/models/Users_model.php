fea1348bdf0a927fbb33834346963e1655f91c41
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-06-21 14:51:06 +0100

    aluno toques finais

--- application/models/Users_model.php
+++ application/models/Users_model.php
@@ -846,7 +846,8 @@ class Users_model extends CI_model{
 				  join disciplinas_licenciatura as dl on dl.id = adl.id_disciplina_licenciatura
 				  join alunos_inscritos_licenciatura as ail on ail.id_disciplina = dl.id
 					  where aac.data = adl.data and (aac.hora > adl.hora_inicial and aac.hora < adl.hora_final) and sentido = 'Entrada' and ail.id_aluno = alunos.id and adl.id=$id_aula and alunos.id = $id_aluno;";
-	    $result = $this->db->query($sql);
+	    $query = $this->db->query($sql);
+	    $result = $query->result();
 	    if(isset($result)) return $result->num;
 	    return 0;
     }
@@ -862,7 +863,8 @@ class Users_model extends CI_model{
 				  join disciplinas_mestrado as dl on dl.id = adl.id_disciplina_mestrado
 				  join alunos_inscritos_mestrado as ail on ail.id_disciplina = dl.id
 					  where aac.data = adl.data and (aac.hora > adl.hora_inicial and aac.hora < adl.hora_final) and sentido = 'Entrada' and ail.id_aluno = alunos.id and adl.id=$id_aula and alunos.id = $id_aluno;";
-	    $result = $this->db->query($sql);
+	    $query = $this->db->query($sql);
+	    $result = $query->result();;
 	    if(isset($result)) return $result->num;
 	    return 0;
     }
@@ -878,7 +880,8 @@ class Users_model extends CI_model{
 				  join disciplinas_pos_graduacoes as dl on dl.id = adl.id_disciplina_pos_graduacao
 				  join alunos_inscritos_pos_graduacoes as ail on ail.id_disciplina = dl.id
 					  where aac.data = adl.data and (aac.hora > adl.hora_inicial and aac.hora < adl.hora_final) and sentido = 'Entrada' and ail.id_aluno = alunos.id and adl.id=$id_aula and alunos.id = $id_aluno;";
-	    $result = $this->db->query($sql);
+	    $query = $this->db->query($sql);
+	    $result = $query->result();
 	    if(isset($result)) return $result->num;
 	    return 0;
     }
