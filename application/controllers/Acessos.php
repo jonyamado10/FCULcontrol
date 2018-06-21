b7533cffa6aae299f57938e97e3db77872b90126
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-06-21 20:58:21 +0100

    aluno toques finais

--- application/controllers/Acessos.php
+++ application/controllers/Acessos.php
@@ -37,11 +37,23 @@ class Acessos extends CI_Controller{
 		$this->form_validation->set_rules('num_acessos', 'num_acessos', 'required|trim');
 
 		if($this->form_validation->run()){
-			if($this->Acessos_model->num_aluno_existe(intval($this->input->post("num_aluno")))){
-			echo $this->input->post("num_aluno");
+			   if($this->Acessos_model->gerar_acessos_detalhados()){
+			    $sucess = "Acessos gerados com sucesso!";
+	          	echo $sucess;
+	           
+			   }
+			   else{
+			   	echo "Erro interno, a gerar Acessos, Tente novamente!";
+			   	return false;
+			   }
 
 		}
-		else{ echo "nao entro";}
+		
+		else{
+
+ 			echo "Todos os Campos tem que ser preenchidos, Tente novamente!";
+			return false;
+		}
 
 	}
 		public function acessos_detalhados_aluno_validation(){
@@ -54,16 +66,21 @@ class Acessos extends CI_Controller{
 		$this->form_validation->set_rules('num_acessos', 'num_acessos', 'required|trim');
 		$this->form_validation->set_rules('num_aluno', 'num_aluno', 'required|trim');
 		if($this->form_validation->run()){
-		   if($this->Acessos_model->gerar_acessos_detalhados_aluno()){
-		    $sucess = "Acessos gerados com sucesso!";
-          	echo $sucess;
-           
-		   }
-		   else{
-		   	echo "Erro interno, a gerar Acessos, Tente novamente!";
-		   	return false;
-		   }
+		   if($this->Acessos_model->num_aluno_existe($this->input->post("num_aluno"))){
+			   if($this->Acessos_model->gerar_acessos_detalhados_aluno()){
+			    $sucess = "Acessos gerados com sucesso!";
+	          	echo $sucess;
+	           
+			   }
+			   else{
+			   	echo "Erro interno, a gerar Acessos, Tente novamente!";
+			   	return false;
+			   }
 
+			}
+			else{
+				echo "Número de Aluno Introduzido não existe.";
+			}
 		}
 		else{
 
