a1549dd5408470ddc3ec9424840291c014152836
unknown <fabionog97@gmail.com>
2018-06-21 16:06:05 +0100

    sair da sessao docente mais olá 3

--- application/views/footer_docente.php
+++ application/views/footer_docente.php
@@ -16,9 +16,9 @@
     <!-- Logout Modal-->
     <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div class="modal-dialog" role="document">
-        <div class="modal-content" style="vertical-align: middle">
+        <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Tem a certeza que pretende sair?</h5>
+            <h5 class="modal-title" id="exampleModalLabel" style="text-align:center;">Tem a certeza que pretende sair?</h5>
 
           </div>
           <div class="modal-footer">
