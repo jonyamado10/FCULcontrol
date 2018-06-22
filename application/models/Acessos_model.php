dab7f4b208f5fdc0d854fc7fe1edbb8417b641bc
João Miguel Dias Amado <fc42191@alunos.fc.ul.pt>
2018-06-22 03:41:22 +0100

    aluno toques finais

--- application/models/Acessos_model.php
+++ application/models/Acessos_model.php
@@ -2201,7 +2201,7 @@ class Acessos_model extends CI_Model {
 		}
 		public function sensores_avariados(){
 			date_default_timezone_set("Europe/Lisbon"); 
-			$umMesAntes = date("Y-m-d",strtotime("-1 month"));
+			$umMesAntes = date("Y-m-d",strtotime("-1 week"));
 			$sql = "SELECT id from sensores
 				WHERE  not EXISTS (SELECT *
                   					FROM   acessos
